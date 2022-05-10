4204bb43e52280ad560a7b8019d0599492a75ce4
arthurfms <arthurfernandes1993@gmail.com>
2022-05-10 17:32:49 +0200

    changed some information on slides

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -24,9 +23,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "flex";
 }
