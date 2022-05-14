0947df769e445b72df81e1c18ab2f6ff21f5104a
Arthur Santos <arthurfernandes1993@gmail.com>
2022-05-14 22:35:32 +0200

    changed the regex to verify email

--- js/form.js
+++ js/form.js
@@ -160,7 +160,7 @@ function notnull(a) {
 // temp
 function ValidateEmail(input) {
   var validRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (input.match(validRegex)) {
     return true;
   } else {
