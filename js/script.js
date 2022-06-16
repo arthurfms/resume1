6d83c347d737125a3cabfd3d427424e1680602ba
arthurfms <arthurfernandes1993@gmail.com>
2022-06-16 17:48:58 +0200

    Recreated the slideshow fully with JS, added a slide template to HTML and make some changes on slide properties for CSS

--- js/script.js
+++ js/script.js
@@ -5,37 +5,9 @@ var speed = 125;
 
 setTimeout(function typeWriter() {
   if (i < text.length) {
-    document.getElementById("header__subtitle-text").innerHTML +=
-      text.charAt(i);
+    document.querySelector(".header__subtitle-text").innerHTML += text.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }, 1500);
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "flex";
-}
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
