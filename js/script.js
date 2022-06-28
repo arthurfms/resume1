f30be03f18e1ccf1bdc4fe0a33ec1ab1a7af3ee3
Arthur Santos <arthurfernandes1993@gmail.com>
2022-06-28 22:08:42 +0200

    created skills section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-function scrolling () {
-    const menu = document.querySelector(".menu");
-    if (window.scrollY != 0) {
-        menu.classList.add("menu_active");
-    } else if (window.scrollY == 0) {
-        menu.classList.remove("menu_active");
-    }
+function scrolling() {
+  const menu = document.querySelector(".menu");
+  if (window.scrollY != 0) {
+    menu.classList.add("menu_active");
+  } else if (window.scrollY == 0) {
+    menu.classList.remove("menu_active");
+  }
 }
 
 window.addEventListener("scroll", scrolling);
 
+window.onload = function () {
+  const skills = document.querySelector(".main-skills");
+
+  skills.addEventListener("click", skillsOnFocus);
+
+  function skillsOnFocus() {
+    console.log("worked");
+    skills.querySelectorAll(".skill").forEach(function (item) {
+      let width = item.querySelector(".skill__percentage").textContent;
+      item.querySelector(".skill-bar__progress").style = `width: ${width}`;
+    });
+  }
+};
