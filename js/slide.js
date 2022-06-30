e1af9aa7590aaba117a297a9e6162270fc1f561a
Arthur Santos <arthurfernandes1993@gmail.com>
2022-06-30 22:25:56 +0200

    Created course slide section

---
+++ js/slide.js
@@ -0,0 +1,32 @@
+const slides = [
+  {
+    title: "Web Development Bootcamp",
+    subtitle: "Practicum by Yandex. FEBRUARY, 2022 - JANUARY, 2023",
+    content:
+      "10 months Web Development bootcamp, foccusing on all frontend technologies, programming languages and methodologies. The course is separated in sprints, trying to generate a real world development enviroment.",
+    skills: ["HTML", "CSS", "JavaScript", "BEM Methodology", "React"],
+  },
+  {
+    title: "Frontend Engineering",
+    subtitle: "EBAC. JANUARY, 2022 - JANUARY, 2023",
+    content:
+      "A year long course about all frontend engineering framework, passing throughout web design, frontend programming and different profession frameworks.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "Bootstrap",
+      "JavaScript",
+      "JQuery",
+      "React",
+      "Ajax",
+    ],
+  },
+  {
+    title: "Java Object-Oriented Development",
+    subtitle: "Universidade Estadual do Ceará. JANUARY, 2022 - JANUARY, 2023",
+    content:
+      "A year long course about all frontend engineering framework, passing throughout web design, frontend programming and different profession frameworks.",
+    skills: ["Java"],
+  },
+];
