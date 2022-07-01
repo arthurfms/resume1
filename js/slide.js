18cd279b4d25a6dd07fa8798d4711b3e4095d2a0
arthurfms <arthurfernandes1993@gmail.com>
2022-07-01 15:03:25 +0200

    Reestructured slide section

--- js/slide.js
+++ js/slide.js
@@ -2,14 +2,14 @@ const slides = [
   {
     title: "Web Development Bootcamp",
     subtitle: "Practicum by Yandex. FEBRUARY, 2022 - JANUARY, 2023",
-    content:
+    description:
       "10 months Web Development bootcamp, foccusing on all frontend technologies, programming languages and methodologies. The course is separated in sprints, trying to generate a real world development enviroment.",
     skills: ["HTML", "CSS", "JavaScript", "BEM Methodology", "React"],
   },
   {
     title: "Frontend Engineering",
     subtitle: "EBAC. JANUARY, 2022 - JANUARY, 2023",
-    content:
+    description:
       "A year long course about all frontend engineering framework, passing throughout web design, frontend programming and different profession frameworks.",
     skills: [
       "HTML",
@@ -24,9 +24,38 @@ const slides = [
   },
   {
     title: "Java Object-Oriented Development",
-    subtitle: "Universidade Estadual do Ceará. JANUARY, 2022 - JANUARY, 2023",
-    content:
-      "A year long course about all frontend engineering framework, passing throughout web design, frontend programming and different profession frameworks.",
+    subtitle: "Universidade Estadual do Ceará. MAY, 2020 - SEPTEMBER, 2020",
+    description:
+      "2 courses with two months each about Java development, foccusing on Web Applications and Object-Oriented programming.",
     skills: ["Java"],
-  },
+  }
 ];
+
+const slideSection = document.querySelector(".slide");
+const slideTemplate = document.querySelector("#slide").content;
+const skillTemplate = document.querySelector("#skill").content;
+
+function generateSlide(title, subtitle, description, skills) {
+  const slide = slideTemplate.querySelector(".slide").cloneNode(true);
+  const skillSection = slide.querySelector(".slide__skills");
+
+  slide.querySelector(".slide__title").textContent = title;
+  slide.querySelector(".slide__subtitle").textContent = subtitle;
+  slide.querySelector(".slide__description").textContent = description;
+
+  skills.forEach(function (item) {
+    const skill = skillTemplate.querySelector(".slide__skill").cloneNode(true);
+    skill.querySelector("p").textContent = item;
+    skillSection.append(skill);
+  });
+
+  return slide;
+}
+
+function createSlide (slide) {
+  
+  console.log(generateSlide(slide.title, slide.subtitle, slide.description, slide.skills));
+}
+
+createSlide(slides[0]);
+
